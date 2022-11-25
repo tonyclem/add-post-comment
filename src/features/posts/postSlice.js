b4b361e3321fbf05fa3d5e3cc67bbfcb13a4d957
tonyclem <tonyclem001@gmail.com>
2022-11-25 23:21:28 +0100

    added reaction button, time stamps, and modified others functions

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -1,12 +1,33 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
 
 const initialState = [
   {
     id: '1',
     title: 'Learning Redux Toolkit',
     content: "I've heard good things about the Redux Toolkit",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
-  { id: '2', title: 'Learning slice', content: "I've heard good things about" },
+  {
+    id: '2',
+    title: 'Learning slice',
+    content: "I've heard good things about",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
+  },
 ];
 
 const postsSlice = createSlice({
@@ -23,16 +44,31 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
